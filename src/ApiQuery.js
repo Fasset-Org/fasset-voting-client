993ca084c94b554e69a8c9156123e36388443caf
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-07-18 15:32:31 +0200

    Update ApiQuery.js

--- src/ApiQuery.js
+++ src/ApiQuery.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-const BASE_API_URL = `http://localhost:8001/api/v1`
+const BASE_API_URL = `http://localhost:8001/api/v1`;
 
 const ApiQuery = {
   getAllCategories: async () => {
-    const resp = await axios.get(
-      `${BASE_API_URL}/getAllCategories`
-    );
+    const resp = await axios.get(`${BASE_API_URL}/getAllCategories`);
 
     return resp?.data;
   },
@@ -18,10 +16,18 @@ const ApiQuery = {
   },
 
   castVote: async (formData) => {
-    const resp = await axios.post(
-      `${BASE_API_URL}/voteCategory`,
-      formData
-    );
+    const resp = await axios.post(`${BASE_API_URL}/voteCategory`, formData);
+
+    return resp?.data;
+  },
+  getUserVotes: async (email) => {
+    const resp = await axios.get(`${BASE_API_URL}/getUserVotes/${email}`);
+
+    return resp?.data;
+  },
+
+  getAllVotes: async () => {
+    const resp = await axios.get(`${BASE_API_URL}/getAllVotes`);
 
     return resp?.data;
   }
