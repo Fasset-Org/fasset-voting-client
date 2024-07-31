a0a8a3f47d7eee0cf2c14d6646aa459072e81785
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-07-31 18:24:31 +0200

    Update Vote.jsx

--- src/pages/Vote/Vote.jsx
+++ src/pages/Vote/Vote.jsx
@@ -143,7 +143,8 @@ const Vote = () => {
               accounts[0].username === "Apatame.Rajabu@fasset.org.za" ||
               accounts[0].username === "Banele.Nduli@fasset.org.za" ||
               accounts[0].username === "Ntsebeng.Khoarai@fasset.org.za" ||
-              accounts[0].username === "Thabo.Khwenenyana@fasset.org.za") && (
+              accounts[0].username === "Thabo.Khwenenyana@fasset.org.za" ||
+              accounts[0].username === "Themba.Makamu@fasset.org.za") && (
               <Tab label="Results" {...a11yProps(2)} />
             )}
           </Tabs>
@@ -305,9 +306,7 @@ const Vote = () => {
           </Stack> */}
         </CustomTabPanel>
         <CustomTabPanel value={value} index={1}>
-          <Alert color="info">
-            Voting will open soon
-          </Alert>
+          <Alert color="info">Voting will open soon</Alert>
         </CustomTabPanel>
         <CustomTabPanel value={value} index={2}>
           <Results />
